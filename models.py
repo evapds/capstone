f376599e1f06d10b82f10583bc68e13d7ff47cb7
Eva Parth dos Santos <eva.parth@gmail.com>
2020-06-02 11:51:07 +0200

    Updates

--- models.py
+++ models.py
@@ -3,18 +3,19 @@ from sqlalchemy import Column, String, create_engine, Integer
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import json
+import sys
 
 # if os.environ['ENV'] == 'test':
 #   database_path = os.environ['TEST_DATABASE_URL']
 # else:
 #   database_path = os.environ['DATABASE_URL']
 
-database_path = os.environ['DATABASE_URL']
-# database_path = os.environ.get('DATABASE_URL')
+# database_path = os.environ['DATABASE_URL']
+database_path = os.environ.get('DATABASE_URL')
 # if not database_path:
-#     database_name = "capstone"
-#     database_path = "postgres://{}/{}".format(
-#         'localhost:5432', database_name)
+#    database_name = "capstone"
+#    database_path = "postgres://{}/{}".format(
+#        'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -29,7 +30,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    # db.create_all()
 
 
 '''
